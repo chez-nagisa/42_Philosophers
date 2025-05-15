8549b06a070f1f0a759112b1f9e84e979c070f02
Nagisa Hara <nagisa@DESKTOP-IMHB0U6.>
2025-05-15 10:18:12 +0900

    oui

--- src/utils.c
+++ src/utils.c
@@ -12,9 +12,47 @@
 
 #include "../include/philo.h"
 
-void	destroy_all(char *message, t_program *program, pthread_mutex_t *forks)
+int	ft_strlen(const char *s)
 {
 	int	i;
+
+	i = 0;
+		if (s == NULL)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	unsigned long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\r'
+		|| str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+void destroy_all(char *message, t_program *program, pthread_mutex_t *forks)
+{
+	int i;
 
 	i = 0;
 	if (message)
@@ -32,9 +70,9 @@ void	destroy_all(char *message, t_program *program, pthread_mutex_t *forks)
 	}
 }
 // usleepはマイクロ秒単位でスリープから、指定時間が長いと誤差が大きい。そのため自作する。
-int	ft_usleep(size_t ms)
+int ft_usleep(size_t ms)
 {
-	size_t	start_time;
+	size_t start_time;
 
 	// 現在の時刻を取得
 	start_time = get_time();
@@ -43,13 +81,13 @@ int	ft_usleep(size_t ms)
 		usleep(500);
 	return (0);
 }
-size_t	get_ms_time(void)
+size_t get_ms_time(void)
 {
-	struct timeval	time;
+	struct timeval time;
 
-	//現在時刻を秒（s）とマイクロ秒（μs）で取得
-	if (gettimeofday(&time, NULL) == -1) //失敗した場合
+	// 現在時刻を秒（s）とマイクロ秒（μs）で取得
+	if (gettimeofday(&time, NULL) == -1) // 失敗した場合
 		write(2, "Error: gettimeofday failed\n", 28);
-	//秒とマイクロ秒をミリ秒に変換
+	// 秒とマイクロ秒をミリ秒に変換
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
